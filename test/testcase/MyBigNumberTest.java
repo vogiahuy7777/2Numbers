ed56797bba15b28c78f9aafe5a26facf3edabcba
Mr.TomaTo <33390996+vogiahuy7777@users.noreply.github.com>
2018-12-23 13:11:04 +0700

    update

--- test/testcase/MyBigNumberTest.java
+++ test/testcase/MyBigNumberTest.java
@@ -25,135 +25,142 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Admin
- */
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 
-public class MyBigNumberTest implements IReceiver {
-
-    public MyBigNumberTest() {
-    }
-
-    /**
-     * Test of sum method, of class MyBigNumber.
-     */
+public class MyBigNumberTest implements IReceiver{
+    public static int i = 1;
     @Test
-    public void testSum1() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "10";
-            String str2 = "10";
-            String expResult = "20";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
-    }
-
-    @Test
-    public void testSum2() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "99";
-            String str2 = "99";
-            String expResult = "198";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
-    }
-
-    @Test
-    public void testSum3() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "999";
-            String str2 = "99";
-            String expResult = "1098";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
-    }
-
-    @Test
-    public void testSum4() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "99";
-            String str2 = "999";
-            String expResult = "1098";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
-    }
-
-    @Test
-    public void testSum5() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "21";
-            String str2 = "10";
-            String expResult = "31";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+    ////string 1 length equal string 2 length
+    public void testSum_N_1() {
+        System.out.println("Test1");
+        String s1 = "123";
+        String s2 = "123";
+        MyBigNumber instance;
+        instance = new MyBigNumber(this);
+        String expResult = "246";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
     }
     
-        public void testSum6() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "0";
-            String str2 = "123";
-            String expResult = "123";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+    
+    ////string 1 length larger than string 2 length
+    @Test
+    public void testSum_N_2() {
+        System.out.println("Test2");
+        String s1 = "12345";
+        String s2 = "1";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "12346";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
     }
-        
-                public void testSum7() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "123";
-            String str2 = "0";
-            String expResult = "123";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+    
+    ////string 1 length shorter than string 2 length
+    @Test
+    public void testSum_N_3() {
+        System.out.println("Test3");
+        String s1 = "1";
+        String s2 = "1234567";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "1234568";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
     }
-
+    
+    ////sum and write mem 1 before result
+    @Test
+    public void testSum_N_4() {
+        System.out.println("Test4");
+        String s1 = "99";
+        String s2 = "1";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "100";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+     ////string 1 empty
+    @Test
+    public void testSum_N_5() {
+        System.out.println("Test5");
+        String s1 = "";
+        String s2 = "123";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "123";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+    ////string 2 empty
+    @Test
+    public void testSum_N_6() {
+        System.out.println("Test6");
+        String s1 = "1234";
+        String s2 = "";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "1234";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+     ////both string empty
+    @Test
+    public void testSum_N_7() {
+        System.out.println("Test7");
+        String s1 = "";
+        String s2 = "";
+        MyBigNumber instance = new MyBigNumber(this);
+        String expResult = "0";
+        String result = instance.sum(s1, s2);
+        assertEquals(expResult, result);
+    }
+    
+    ////Test when string1 input contain a letter char
+    @Test(expected=NumberFormatException.class)
+    public void testSum_N_8() {
+        System.out.println("Test8");
+        String s1 = "a12";
+        String s2 = "1234";
+        MyBigNumber instance = new MyBigNumber(this);
+        String result = instance.sum(s1, s2);
+    }
+    
+    ////Test when string2 input contain a letter char
+    @Test(expected=NumberFormatException.class)
+    public void testSum_N_9() {
+        System.out.println("Test9");
+        String s1 = "1234";
+        String s2 = "a";
+        MyBigNumber instance = new MyBigNumber(this);
+        String result = instance.sum(s1, s2);
+    }
+    
+    ////Test when string1 input contain special char
+    @Test(expected=NumberFormatException.class)
+    public void testSum_N_10() {
+        System.out.println("Test9");
+        String s1 = "!!12";
+        String s2 = "1234";
+        MyBigNumber instance = new MyBigNumber(this);
+        String result = instance.sum(s1, s2);
+    }
+    
+    ////Test when string1 input contain special char
+    @Test(expected=NumberFormatException.class)
+    public void testSum_N_11() {
+        System.out.println("Test11");
+        String s1 = "123";
+        String s2 = "!!1";
+        MyBigNumber instance = new MyBigNumber(this);
+        String result;
+        result = instance.sum(s1, s2);
+    }
+    
     @Override
-    public void send(String msg) {
-        System.out.println(msg + "\n");
-    }
+	public void send(String msg) {
+	 System.out.println("Case" + i + ":");
+        i++;
+	}
+
 
 }
